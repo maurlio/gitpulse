4ed87ca1672a896ae84277b7ba5abde43ce6849b
Maurilio Felipe Rodrigues <maurilioofelipe@yahoo.com>
2026-03-03 18:10:05 -0300

    feat: implementar cliente HTTPS usando rustls e ureq

---
+++ src/http.rs
@@ -0,0 +1,19 @@
+use std::io::Read;
+use ureq;
+
+pub struct HttpResponse {
+    pub status: u16,
+    pub body: String,
+}
+
+pub fn get_github_events(username: &str) -> Result<HttpResponse, Box<dyn std::error::Error>> {
+    let url = format!("https://api.github.com/users/{}/events", username);
+
+    let response = ureq::get(&url).set("User-Agent", "gitpulse-cli").call()?;
+    let status = response.status();
+    let mut body = String::new();
+
+    response.into_reader().read_to_string(&mut body)?;
+
+    Ok(HttpResponse { status, body })
+}
