2dae4656d95d3940cbda98bb894cf57c5622f01f
Maurilio Felipe Rodrigues <maurilioofelipe@yahoo.com>
2026-03-03 18:10:05 -0300

    feat: implementa cliente HTTPS usando rustls e ureq

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod cli;
+mod github;
 mod http;
 mod json;
-mod github;
 
-use std::process;
 use cli::Config;
+use std::process;
+use ureq::Response;
 
 fn main() {
     let config = Config::build().unwrap_or_else(|err| {
@@ -12,5 +13,26 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Buscando atividades do usuário: @{}", config.username);
+    println!(
+        "Conectando ao GitHub para obter eventos do usuário: @{}",
+        config.username
+    );
+
+    match http::get_github_events(&config.username) {
+        Ok(response) => {
+            println!("Sucesso! Eventos capturados.");
+        }
+        Err(e) => {
+            if let Some(ureq::Error::Status(code, _)) = e.downcast_ref::<ureq::Error>() {
+                if *code == 404 {
+                    eprintln!("Erro: Usuário '@{}' não encontrado no GitHub.", config.username);
+                } else {
+                    eprintln!("Erro na API do GitHub: Status {}", code);
+                }
+            } else {
+                eprintln!("Erro ao conectar ao GitHub: {}", e);
+            }
+            std::process::exit(1);
+        }
+    }
 }
