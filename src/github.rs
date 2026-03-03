4b6297ddbbfb61ef70e8f227af1ef75b1f93f931
Maurilio Felipe Rodrigues <maurilioofelipe@yahoo.com>
2026-03-03 19:23:27 -0300

    refactor: remover warnings e traduzir mensagens para português

--- src/github.rs
+++ src/github.rs
@@ -1,5 +1,6 @@
 use serde::Deserialize;
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct GitHubEvent {
     #[serde(rename = "type")]
@@ -8,18 +9,22 @@ pub struct GitHubEvent {
     pub payload: Payload,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct Repo {
     pub name: String,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct Payload {
     pub commits: Option<Vec<Commit>>,
     pub action: Option<String>,
     pub ref_type: Option<String>,
+    pub size: Option<u32>, // Adicionado para capturar o número de commits de forma alternativa
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct Commit {
     pub message: String,
